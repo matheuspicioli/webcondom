576a3b29ccf31a441840b69890ec3fe8e484c11f
Matheus Picioli <matheus.picioli98@gmail.com>
2017-10-31 18:52:07 +0000

    EstadoCOD unsigned()

--- database/migrations/2017_10_26_115638_create_cidades_table.php
+++ database/migrations/2017_10_26_115638_create_cidades_table.php
@@ -34,6 +34,8 @@ class CreateCidadesTable extends Migration
      */
     public function down()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('Cidades');
+        Schema::enableForeignKeyConstraints();
     }
 }
