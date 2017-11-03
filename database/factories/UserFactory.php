60268d50ce3faad01988ce8e626e543fcfd81e59
Matheus Picioli <matheus.picioli98@gmail.com>
2017-11-03 18:46:32 +0000

    Fazendo a factory pra poder rodar as seeds

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,13 +13,32 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(WebCondom\User::class, function (Faker $faker) {
-    static $password;
-
+$factory->define(WebCondom\Models\Enderecos\Estado::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'Descricao'     => $faker->name,
+        'Sigla'         => $faker->SP,
+        'CodigoIBGE'    => $faker->randomNumber(2)
+    ];
+});
+
+$factory->define(WebCondom\Models\Enderecos\Cidade::class, function (Faker $faker) {
+    return [
+        'Descricao'     => $faker->name,
+        'CodigoIBGE'    => $faker->randomNumber(7)
+    ];
+});
+
+$factory->define(WebCondom\Models\Condominios\CondominioTaxa::class, function (Faker $faker) {
+    return [
+        'Descricao'     => $faker->name,
+        'Valor'         => $faker->randomNumber(3)
+    ];
+});
+
+$factory->define(WebCondom\Models\Condominios\Sindico::class, function (Faker $faker) {
+    return [
+        'Nome'      => $faker->name,
+        'Telefone'  => $faker->randomNumber(8),
+        'Celular'   => $faker->randomNumber(8)
     ];
 });
