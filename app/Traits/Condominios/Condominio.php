aff77b00c2cdb35699d0bbf0e6df79eba15bd282
Matheus Picioli <matheus.picioli98@gmail.com>
2017-11-06 18:46:10 +0000

    Função que retornava o endereço movida p/ trait de endereço

--- app/Traits/Condominios/Condominio.php
+++ app/Traits/Condominios/Condominio.php
@@ -3,17 +3,13 @@ namespace WebCondom\Traits\Condominios;
 
 trait Condominio
 {
-    public function getEnderecoFormatadoAttribute()
-    {
-        $logradouro = $this->Endereco->Logradouro;
-        $numero     = $this->Endereco->Numero;
-        $bairro     = $this->Endereco->Bairro;
-        $cidade     = $this->Endereco->Cidade->Descricao." - ".$this->Endereco->Cidade->Estado->Sigla;
-        return "$logradouro, $numero, $bairro - $cidade";
-    }
-
     public function getTipoJurosFormatadoAttribute()
     {
         return $this->TipoJuros == "AM" ? "Ao mês" : "Ao dia";
+    }
+
+    public function getCondominioDescricaoAttribute()
+    {
+        return "$this->Nome - $this->Apelido";
     }
 }
