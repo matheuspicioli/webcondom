501ba43981384918b001c4a41e05c7b73dbba379
Matheus Picioli <matheus.picioli98@gmail.com>
2017-11-09 13:11:15 +0000

    Lista de estados como primeiro campo do formulario

--- resources/views/enderecos/cidades/criar.blade.php
+++ resources/views/enderecos/cidades/criar.blade.php
@@ -16,6 +16,17 @@
                                 <hr>
                                 <form method="post" action="{{ route('enderecos.cidades.salvar') }}">
                                     {{ csrf_field() }}
+                                    
+                                    <div class="form-group">
+                                        <label for="EstadoCOD" class="control-label">Estado</label>
+                                        <select name="estado_id" id="EstadoCOD" class="form-control">
+                                            <option selected disabled>-------Selecione um estado-------</option>
+                                            @foreach($estados as $estado)
+                                                <option value="{{ $estado->id }}">{{ $estado->descricao }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
                                     <div class="form-group">
                                         <label for="Descricao" class="control-label">Descrição</label>
                                         <input id="Descricao" type="text" class="form-control" name="descricao">
@@ -25,16 +36,6 @@
                                         <label for="CodigoIBGE" class="control-label">Codigo IBGE</label>
                                         <input type="text" id="CodigoIBGE" name="codigo_ibge" class="form-control">
                                         <span class="help-block">Este campo é opcional</span>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="EstadoCOD" class="control-label">Estado</label>
-                                        <select name="estado_id" id="EstadoCOD" class="form-control">
-                                            <option selected disabled>-------Selecione um estado-------</option>
-                                            @foreach($estados as $estado)
-                                                <option value="{{ $estado->id }}">{{ $estado->descricao }}</option>
-                                            @endforeach
-                                        </select>
                                     </div>
 
                                     <div class="form-group">
--- resources/views/enderecos/cidades/exibir.blade.php
+++ resources/views/enderecos/cidades/exibir.blade.php
@@ -18,6 +18,16 @@
                                     <form method="post" action="{{ route('enderecos.cidades.alterar', ['id' => $cidade->id ]) }}">
                                         {{ csrf_field() }}
                                         <input type="hidden" name="_method" value="PUT">
+
+                                        <div class="form-group">
+                                            <label for="EstadoCOD" class="control-label">Estado</label>
+                                            <select name="estado_id" id="EstadoCOD" class="form-control">
+                                                @foreach($estados as $estado)
+                                                    <option {{ $estado->id == $cidade->estado_id ? 'selected' : '' }} value="{{ $estado->id }}">{{ $estado->descricao }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+
                                         <div class="form-group">
                                             <label for="Descricao" class="control-label">Descrição</label>
                                             <input id="Descricao" type="text" class="form-control" name="descricao" value="{{ $cidade->descricao }}">
@@ -27,15 +37,6 @@
                                             <label for="CodigoIBGE" class="control-label">Codigo IBGE</label>
                                             <input type="text" id="CodigoIBGE" name="codigo_ibge" class="form-control" value="{{ $cidade->codigo_ibge ? $cidade->codigo_ibge : '' }}">
                                             <span class="help-block">Este campo é opcional</span>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="EstadoCOD" class="control-label">Estado</label>
-                                            <select name="estado_id" id="EstadoCOD" class="form-control">
-                                                @foreach($estados as $estado)
-                                                    <option {{ $estado->id == $cidade->estado_id ? 'selected' : '' }} value="{{ $estado->id }}">{{ $estado->descricao }}</option>
-                                                @endforeach
-                                            </select>
                                         </div>
 
                                         <div class="form-group">
