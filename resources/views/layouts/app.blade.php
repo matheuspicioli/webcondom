bb9e140f6b263d2e7597c2851b7ea71b07b9189a
Renan C Sanfelice <renan@emporiumdigital.net>
2017-11-17 11:49:20 -0200

    alterado a fonte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('titulo')</title>
-    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         html, body{
-            font-family: 'Oswald', sans-serif;
+            font-family: 'Roboto', sans-serif;
         }
     </style>
 </head>
