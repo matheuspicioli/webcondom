2339f2a44f7e315a49cb08ee18313dbc4a62333d
Matheus Picioli <matheus.picioli98@gmail.com>
2017-11-18 13:25:53 +0000

    Importando os components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,12 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('pagina', require('./components/Pagina.vue'));
+Vue.component('painel', require('./components/Painel.vue'));
+Vue.component('formulario', require('./components/Formulario.vue'));
+Vue.component('modal', require('./components/modal/Modal.vue'));
+Vue.component('modal-link', require('./components/modal/ModalLink.vue'));
+Vue.component('migalha', require('./components/Migalhas.vue'));
 
 const app = new Vue({
     el: '#app'
