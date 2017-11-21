4703cc51267a1654f93e5829178a1c0edb977c9f
Matheus Picioli <matheus.picioli98@gmail.com>
2017-11-21 17:58:11 +0000

    Removendo modal

--- resources/views/enderecos/estados/listar.blade.php
+++ resources/views/enderecos/estados/listar.blade.php
@@ -4,7 +4,6 @@
     <pagina tamanho="12">
         <painel titulo="Lista de estados" cor="panel-primary">
             <migalha v-bind:lista="{{ $migalhas }}"></migalha>
-            <modal-link tipo="button" nome="modalCadastraEstado" titulo="Modal cadastra estado"></modal-link>
             <a href="{{ route('enderecos.estados.criar') }}" class="btn btn-primary">Cadastrar</a>
             <hr />
             <table class="table table-bordered">
@@ -29,29 +28,4 @@
             </table>
         </painel>
     </pagina>
-    <modal nome="modalCadastraEstado">
-        <painel cor="panel-primary" titulo="Cadastrar estado">
-            <formulario action="{{ route('enderecos.estados.salvar') }}" method="POST" token="{{ csrf_token() }}">
-                <div class="form-group">
-                    <label for="Descricao" class="control-label">Descrição</label>
-                    <input id="Descricao" type="text" class="form-control" name="descricao">
-                </div>
-
-                <div class="form-group">
-                    <label for="Sigla" class="control-label">Sigla</label>
-                    <input type="text" id="Sigla" name="sigla" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="CodigoIBGE" class="control-label">Código IBGE</label>
-                    <input type="text" id="CodigoIBGE" name="codigo_ibge" class="form-control">
-                    <span class="help-block">Este campo é opcional</span>
-                </div>
-
-                <div class="form-group">
-                    <button class="btn btn-primary" type="submit">Cadastrar</button>
-                </div>
-            </formulario>
-        </painel>
-    </modal>
 @endsection
