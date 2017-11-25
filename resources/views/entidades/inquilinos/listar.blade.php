640fd979964f88aff37a17c6d616cb754a72454a
Matheus Picioli <matheus.picioli98@gmail.com>
2017-11-25 11:03:02 +0000

    Views de inquilinos (ainda nao esta funcionando

---
+++ resources/views/entidades/inquilinos/listar.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@section('titulo', 'Estado Civil - Lista')
+@section('conteudo')
+    <pagina tamanho="10">
+        <painel cor="panel-primary" titulo="Lista de Estado Civis">
+            <migalha v-bind:lista="{{ $migalhas }}"></migalha>
+            <a href="{{ route('diversos.estadoCivil.criar') }}" class="btn btn-primary">Cadastrar</a>
+            <hr>
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Descrição</th>
+                    <th>Conjuge?</th>
+                    <th>Ações</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($estadoCivil as $estadosCivis)
+                    <tr>
+                        <td>{{ $estadosCivis->id }}</td>
+                        <td>{{ $estadosCivis->descricao }}</td>
+                        <td>{{ $estadosCivis->exige_conjuge_formatado }}</td>
+                        <td>
+                            <a class="btn btn-success"
+                               href="{{ route('diversos.estadoCivil.exibir', ['id' => $estadosCivis->id]) }}">Alterar</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </painel>
+    </pagina>
+@endsection
