614190f2e8cebc18a92f6e0ec8126758eb21b78c
Matheus Picioli <matheus.picioli98@gmail.com>
2017-12-04 11:44:23 +0000

    Adicionando relacionamento pra gerar dados automaticamente

--- app/Models/Condominios/Imovel.php
+++ app/Models/Condominios/Imovel.php
@@ -3,9 +3,12 @@
 namespace WebCondom\Models\Condominios;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Imovel extends Model
 {
+    use SoftDeletes;
+    
     protected $table = "imoveis";
     protected $fillable = [
         "codigo", "referencia", "endereco_id", "tipo_imovel_id",
@@ -18,13 +21,18 @@ class Imovel extends Model
         return $this->belongsTo('WebCondom\Models\Enderecos\Endereco');
     }
 
-    public function condominio()
+    public function tipo_imovel()
     {
-        return $this->belongsTo('WebCondom\Models\Condominios\Condominio');
+        return $this->belongsTo('WebCondom\Models\Diversos\TipoImovel');
     }
 
     public function categoria()
     {
         return $this->belongsTo('WebCondom\Models\Diversos\Categoria');
+    }
+
+    public function condominio()
+    {
+        return $this->belongsTo('WebCondom\Models\Condominios\Condominio');
     }
 }
