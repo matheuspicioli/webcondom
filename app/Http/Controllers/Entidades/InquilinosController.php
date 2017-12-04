5b5aeac9fb94e4b1baae911684723274fef3e2ee
Matheus Picioli <matheus.picioli98@gmail.com>
2017-12-04 18:22:11 +0000

    Controller inquilinos

---
+++ app/Http/Controllers/Entidades/InquilinosController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace WebCondom\Http\Controllers\Entidades;
+
+use Illuminate\Http\Request;
+use WebCondom\Http\Controllers\Controller;
+use WebCondom\Models\Diversos\EstadoCivil;
+use WebCondom\Models\Diversos\RegimeCasamento;
+use WebCondom\Models\Enderecos\Cidade;
+use WebCondom\Models\Entidades\Inquilino;
+
+class InquilinosController extends Controller
+{
+    public function Listar()
+    {
+        $inquilinos = Inquilino::all();
+        $migalhas = json_encode([
+            ['titulo' => 'Home', 'url' => route('home')],
+            ['titulo' => 'Inquilinos', 'url' => '']
+        ]);
+        return view('entidades.inquilinos.listar', compact('inquilinos', 'migalhas'));
+    }
+
+    public function Criar()
+    {
+        $migalhas = json_encode([
+            ['titulo' => 'Home', 'url' => route('home')],
+            ['titulo' => 'Inquilinos', 'url' => route('entidades.inquilinos.listar')],
+            ['titulo' => 'Cadastrar inquilino', 'url' => '']
+        ]);
+        $estados_civis = EstadoCivil::all();
+        $regimes_casamentos = RegimeCasamento::all();
+        $cidades = Cidade::all();
+        return view('entidades.inquilinos.criar', compact('estados_civis', 'regimes_casamentos', 'cidades', 'migalhas'));
+    }
+
+    public function Salvar(Request $request)
+    {
+        $inquilino = Inquilino::create($request->all());
+        $entidade = $inquilino->entidade()->create($request->all());
+        $enderecoPrincipal = $entidade->endereco_principal()->create([
+            'logradouro'    => $request->input('logradouro_principal'),
+            'numero'        => $request->input('numero_principal'),
+            'cep'           => $request->input('cep_principal'),
+            'bairro'        => $request->input('bairro_principal'),
+            'cidade_id'     => $request->input('cidade_id_principal'),
+        ]);
+        $enderecoCobranca = $entidade->endereco_cobranca()->create([
+            'logradouro'    => $request->input('logradouro_cobranca'),
+            'numero'        => $request->input('numero_cobranca'),
+            'cep'           => $request->input('cep_cobranca'),
+            'bairro'        => $request->input('bairro_cobranca'),
+            'cidade_id'     => $request->input('cidade_id_cobranca'),
+        ]);
+        $entidade->endereco_principal()->associate($enderecoPrincipal);
+        $entidade->endereco_cobranca()->associate($enderecoCobranca);
+        $inquilino->entidade()->associate($entidade);
+        $entidade->save();
+        $inquilino->save();
+
+        $request->session()->flash('sucesso', 'Inquilino criado com sucesso!');
+        return redirect()->route('entidades.inquilinos.listar');
+    }
+
+    public function Exibir($id)
+    {
+        $migalhas = json_encode([
+            ['titulo' => 'Home', 'url' => route('home')],
+            ['titulo' => 'Inquilinos', 'url' => route('entidades.inquilinos.listar')],
+            ['titulo' => 'Alterar inquilino', 'url' => '']
+        ]);
+        $inquilino = Inquilino::find($id) ? Inquilino::find($id) : null;
+
+        if ($inquilino) {
+            $estados_civis = EstadoCivil::all();
+            $regimes_casamentos = RegimeCasamento::all();
+            $cidades = Cidade::all();
+            return view('entidades.inquilinos.exibir', compact('estados_civis', 'regimes_casamentos', 'inquilino', 'cidades', 'migalhas'));
+        } else
+            return redirect()->route('entidades.inquilinos.criar', 'migalhas');
+    }
+
+    public function Alterar(Request $request, $id)
+    {
+        $inquilino = Inquilino::find($id);
+        $inquilino->update($request->all());
+        $inquilino->entidade()->update($request->all());
+        //----ENDEREÇO PRINCIPAL---//
+        $inquilino->entidade->endereco_principal()->update([
+            'logradouro'    => $request->input('logradouro_principal'),
+            'numero'        => $request->input('numero_principal'),
+            'cep'           => $request->input('cep_principal'),
+            'bairro'        => $request->input('bairro_principal'),
+            'cidade_id'     => $request->input('cidade_id_principal')
+        ]);
+        //----ENDEREÇO COBRANÇA---//
+        $inquilino->entidade->endereco_cobranca()->update([
+            'logradouro'    => $request->input('logradouro_cobranca'),
+            'numero'        => $request->input('numero_cobranca'),
+            'cep'           => $request->input('cep_cobranca'),
+            'bairro'        => $request->input('bairro_cobranca'),
+            'cidade_id'     => $request->input('cidade_id_cobranca')
+        ]);
+        //dd($inquilino->entidade->endereco_cobranca);
+        //SALVA E SALVA OS RELACIONAMENTOS TAMBÉM
+        $inquilino->push();
+
+        $request->session()->flash('info', 'Inquilino alterado com sucesso!');
+        return redirect()->route('entidades.inquilinos.listar');
+    }
+
+    public function Excluir(Request $request, $id)
+    {
+        Inquilino::find($id)->delete();
+        $request->session()->flash('warning', 'Inquilino deletado com sucesso!');
+        return redirect()->route('entidades.inquilinos.listar');
+    }
+}
