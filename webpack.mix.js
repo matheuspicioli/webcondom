e6ffd61131124220e98cb748e8611cb6a1ea7446
Matheus Picioli <matheus.picioli98@gmail.com>
2017-12-08 11:37:16 +0000

    removendo a função copy do plugin mask jquery e validation

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,10 +10,9 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.copy('node_modules/bootstrap-sass/', 'resources/assets/sass/bootstrap-sass/');
-mix.copy('node_modules/jquery-mask-plugin/', 'public/js/jquery-mask-plugin/');
-mix.copy('node_modules/vue-material/dist/vue-material.css', 'public/css/vue-material/');
-mix.copy('node_modules/vue-material/dist/vue-material.css.map', 'public/css/vue-material/');
+//mix.copy('node_modules/bootstrap-sass/', 'resources/assets/sass/bootstrap-sass/');
+//mix.copy('node_modules/vue-material/dist/vue-material.css', 'public/css/vue-material/');
+//mix.copy('node_modules/vue-material/dist/vue-material.css.map', 'public/css/vue-material/');
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/bootstrap-sass/assets/stylesheets/_bootstrap.scss', 'public/css');
