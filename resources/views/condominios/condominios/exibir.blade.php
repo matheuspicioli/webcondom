e61a90aadf783d5508cc4419cc336f88ed3665f4
Matheus Picioli <matheus.picioli98@gmail.com>
2017-12-08 11:41:02 +0000

    Adicionando os campos de endereço dentro de um painel-info e removendo o span help-block

--- resources/views/condominios/condominios/exibir.blade.php
+++ resources/views/condominios/condominios/exibir.blade.php
@@ -2,7 +2,7 @@
 @section('titulo', 'Condomínios - Exibir/Alterar')
 @section('conteudo')
     <pagina tamanho="12">
-        <painel titulo="Alterar condomínio" cor="panel-primary">
+        <painel titulo="Alterar condomínio" cor="panel-primary" posicao="text-center">
             <div class="row">
                 <div class="col-md-1">
                     <a href="{{ route('condominios.condominios.listar') }}" class="btn btn-default">Voltar</a>
@@ -36,7 +36,6 @@
                             <label for="Telefone" class="control-label">Telefone</label>
                             <input id="Telefone" type="text" class="form-control pula" name="telefone"
                                    value="{{ $condominio->telefone ? $condominio->telefone : '' }}">
-                            <span class="help-block">Este campo é opcional</span>
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -61,7 +60,6 @@
                             <label for="Multa" class="control-label">Multa (%)</label>
                             <input id="Multa" type="text" class="form-control pula" name="multa" placeholder="150,00"
                                    value="{{ $condominio->multa ? $condominio->multa : '' }}">
-                            <span class="help-block">Este campo é opcional</span>
                         </div>
                     </div>
                 </div>
@@ -72,7 +70,6 @@
                             <label for="Juros" class="control-label">Juros (%)</label>
                             <input id="Juros" type="text" class="form-control pula" name="juros" placeholder="150,00"
                                    value="{{ $condominio->juros ? $condominio->juros : '' }}">
-                            <span class="help-block">Este campo é opcional</span>
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -125,66 +122,63 @@
                         </div>
                     </div>
                 </div>
-                <hr>
-                {{-- PARTE FORMULÁRIO ENDEREÇO --}}
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="CEP" class="control-label">CEP</label>
-                            <input type="text" id="CEP" name="cep" class="form-control pula"
-                                   value="{{ $condominio->endereco->cep }}">
-                            <span class="help-block">Apenas os números</span>
+                <painel titulo="Endereço" cor="panel-info" posicao="text-center">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="CEP" class="control-label">CEP</label>
+                                <input type="text" id="CEP" name="cep" class="form-control pula"
+                                       value="{{ $condominio->endereco->cep }}">
+                            </div>
+                        </div>
+
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <label for="Logradouro" class="control-label">Logradouro</label>
+                                <input id="Logradouro" type="text" class="form-control pula" name="logradouro"
+                                       value="{{ $condominio->endereco->logradouro }}">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="Numero" class="control-label">Número</label>
+                                <input type="number" min="0" id="Numero" name="numero" class="form-control pula"
+                                       value="{{ $condominio->endereco->numero }}">
+                            </div>
+                        </div>
+                        <div class="col-md-10">
+                            <div class="form-group">
+                                <label for="Complemento" class="control-label">Complemento</label>
+                                <input type="text" id="Complemento" name="complemento" class="form-control pula"
+                                       value="{{ $condominio->endereco->complemento ? $condominio->endereco->complemento : '' }}">
+                            </div>
                         </div>
                     </div>
 
-                    <div class="col-md-8">
-                        <div class="form-group">
-                            <label for="Logradouro" class="control-label">Logradouro</label>
-                            <input id="Logradouro" type="text" class="form-control pula" name="logradouro"
-                                   value="{{ $condominio->endereco->logradouro }}">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="Bairro" class="control-label">Bairro</label>
+                                <input type="text" id="Bairro" name="bairro" class="form-control pula"
+                                       value="{{ $condominio->endereco->bairro }}">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <label for="CidadeCOD" class="control-label">Cidade</label>
+                                <select name="cidade_id" id="CidadeCOD" class="form-control pula">
+                                    <option selected disabled>-------Selecione uma cidade-------</option>
+                                    @foreach($cidades as $cidade)
+                                        <option value="{{ $cidade->id }}" {{ $cidade->id == $condominio->endereco->cidade->id ? 'selected' : '' }}>{{ $cidade->descricao }}
+                                            - {{ $cidade->estado->descricao }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-2">
-                        <div class="form-group">
-                            <label for="Numero" class="control-label">Número</label>
-                            <input type="number" min="0" id="Numero" name="numero" class="form-control pula"
-                                   value="{{ $condominio->endereco->numero }}">
-                        </div>
-                    </div>
-                    <div class="col-md-10">
-                        <div class="form-group">
-                            <label for="Complemento" class="control-label">Complemento</label>
-                            <input type="text" id="Complemento" name="complemento" class="form-control pula"
-                                   value="{{ $condominio->endereco->complemento ? $condominio->endereco->complemento : '' }}">
-                            <span class="help-block">Este campo é opcional</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="Bairro" class="control-label">Bairro</label>
-                            <input type="text" id="Bairro" name="bairro" class="form-control pula"
-                                   value="{{ $condominio->endereco->bairro }}">
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="form-group">
-                            <label for="CidadeCOD" class="control-label">Cidade</label>
-                            <select name="cidade_id" id="CidadeCOD" class="form-control pula">
-                                <option selected disabled>-------Selecione uma cidade-------</option>
-                                @foreach($cidades as $cidade)
-                                    <option value="{{ $cidade->id }}" {{ $cidade->id == $condominio->endereco->cidade->id ? 'selected' : '' }}>{{ $cidade->descricao }}
-                                        - {{ $cidade->estado->descricao }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <hr/>
+                </painel>
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group">
