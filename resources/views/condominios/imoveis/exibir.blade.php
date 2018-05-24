24bc08ba7f7cae60e614c333c6c7a0eede35d385
Matheus Picioli <matheus.picioli@ciatecnica.com.br>
2018-05-24 15:24:26 -0300

    Bloco de endereço virou component

--- resources/views/condominios/imoveis/exibir.blade.php
+++ resources/views/condominios/imoveis/exibir.blade.php
@@ -43,7 +43,9 @@
                                     <div class="form-group">
                                         <label for="Codigo" class="control-label" @if($errors->has('codigo')) style="color: #f56954" @endif>Código</label>
                                         <input id="Codigo" type="text" class="form-control pula" name="codigo"
-                                               data-mask="999999" value="{{ old('codigo') ? old('codigo') : $imovel->codigo }}" @if($errors->has('codigo')) style="border:1px solid #f56954" @endif>
+                                               data-mask="999999" value="{{ old('codigo') ? old('codigo') : $imovel->codigo }}"
+											   tabindex="1"
+											   @if($errors->has('codigo')) style="border:1px solid #f56954" @endif>
                                         @if( $errors->has('codigo') )
                                             <span style="color: #f56954">{{ $errors->get('codigo')[0] }}</span>
                                         @endif
@@ -53,6 +55,7 @@
                                     <div class="form-group">
 										<label for="referencia" class="control-label" @if($errors->has('referencia')) style="color: #f56954" @endif>Referência</label>
 										<input id="referencia" type="text" class="form-control pula" name="referencia"
+											   tabindex="2"
 											   value="{{ old('referencia') ? old('referencia') : $imovel->referencia }}" @if($errors->has('referencia')) style="border:1px solid #f56954" @endif>
 										@if( $errors->has('referencia') )
 											<span style="color: #f56954">{{ $errors->get('referencia')[0] }}</span>
@@ -62,7 +65,7 @@
                                 <div class="col-md-3">
                                     <div class="form-group">
                                         <label for="tipo_imovel" class="control-label" @if($errors->has('tipo_imovel_id')) style="color: #f56954" @endif>Tipo imóvel</label>
-                                        <select id="tipo_imovel" class="form-control pula" name="tipo_imovel_id">
+                                        <select id="tipo_imovel" class="form-control pula select2" name="tipo_imovel_id" tabindex="3">
                                             @foreach($tiposImoveis as $tipoImovel)
 												<option value="{{ $tipoImovel->id }}" {{ old('tipo_imovel_id') == $tipoImovel->id ? 'selected' : ($tipoImovel->id == $imovel->tipo_imovel_id ? 'selected' : '') }}>
 													{{ $tipoImovel->descricao }}
@@ -77,7 +80,7 @@
                                 <div class="col-md-3">
                                     <div class="form-group">
 										<label for="categoria" class="control-label" @if($errors->has('categoria')) style="color: #f56954" @endif>Categoria</label>
-                                        <select id="categoria" class="form-control pula" name="categoria_id">
+                                        <select id="categoria" class="form-control pula select2" name="categoria_id" tabindex="4">
                                             @foreach($categorias as $categoria)
 												<option value="{{ $categoria->id }}" {{ old('categoria_id') == $categoria->id ? 'selected' : ($categoria->id == $imovel->categoria_id ? 'selected' : '') }}>
 													{{ $categoria->descricao }}
@@ -94,7 +97,7 @@
                                 <div class="col-md-4">
                                     <div class="form-group">
 										<label for="condominio" class="control-label" @if($errors->has('condominio_id')) style="color: #f56954" @endif>Condomínio</label>
-                                        <select id="condominio" class="form-control pula" name="condominio_id">
+                                        <select id="condominio" class="form-control pula select2" name="condominio_id" tabindex="5">
                                             @foreach($condominios as $condominio)
 												<option value="{{ $condominio->id }}" {{ old('condominio_id') == $condominio->id ? 'selected' : ($condominio->id == $imovel->condominio_id ? 'selected' : '') }}>
 													{{ $condominio->nome }}
@@ -110,6 +113,7 @@
                                     <div class="form-group">
 										<label for="valor_locacao" class="control-label" @if($errors->has('valor_locacao')) style="color: #f56954" @endif>Valor de locação</label>
                                         <input id="valor_locacao" type="text" class="form-control pula" name="valor_locacao" placeholder="0,00"
+											   tabindex="6"
                                                value="{{ old('valor_locacao') ? old('valor_locacao') : $imovel->valor_locacao_view }}">
 										@if( $errors->has('valor_locacao') )
 											<span style="color: #f56954">{{ $errors->get('valor_locacao')[0] }}</span>
@@ -120,6 +124,7 @@
                                     <div class="form-group">
 										<label for="valor_venda" class="control-label" @if($errors->has('valor_venda')) style="color: #f56954" @endif>Valor de venda</label>
                                         <input id="valor_venda" type="text" class="form-control pula" name="valor_venda" placeholder="0,00"
+											   tabindex="7"
                                                value="{{ old('valor_venda') ? old('valor_venda') : $imovel->valor_venda_view }}">
 										@if( $errors->has('valor_venda') )
 											<span style="color: #f56954">{{ $errors->get('valor_venda')[0] }}</span>
@@ -132,6 +137,7 @@
                                     <div class="form-group">
 										<label for="codigo_agua" class="control-label" @if($errors->has('codigo_agua')) style="color: #f56954" @endif>Código água</label>
                                         <input id="codigo_agua" type="text" class="form-control pula" maxlength="20"
+											   tabindex="8"
                                                name="codigo_agua" value="{{ old('valor_venda') ? old('valor_venda') : $imovel->codigo_agua }}">
 										@if( $errors->has('codigo_agua') )
 											<span style="color: #f56954">{{ $errors->get('codigo_agua')[0] }}</span>
@@ -142,6 +148,7 @@
                                     <div class="form-group">
 										<label for="codigo_iptu" class="control-label" @if($errors->has('codigo_iptu')) style="color: #f56954" @endif>Código IPTU</label>
                                         <input id="codigo_iptu" type="text" class="form-control pula" maxlength="20"
+											   tabindex="9"
                                                name="codigo_iptu" value="{{ old('codigo_iptu') ? old('codigo_iptu') : $imovel->codigo_iptu }}">
 										@if( $errors->has('codigo_iptu') )
 											<span style="color: #f56954">{{ $errors->get('codigo_iptu')[0] }}</span>
@@ -152,6 +159,7 @@
                                     <div class="form-group">
 										<label for="codigo_energia" class="control-label" @if($errors->has('codigo_energia')) style="color: #f56954" @endif>Código água</label>
                                         <input id="codigo_energia" type="text" class="form-control pula" maxlength="20"
+											   tabindex="10"
                                                name="codigo_energia" value="{{ old('codigo_energia') ? old('codigo_energia') : $imovel->codigo_energia }}">
 										@if( $errors->has('codigo_energia') )
 											<span style="color: #f56954">{{ $errors->get('codigo_energia')[0] }}</span>
@@ -164,6 +172,7 @@
                                     <div class="form-group">
 										<label for="descritivo" class="control-label" @if($errors->has('descritivo')) style="color: #f56954" @endif>Descritivo</label>
                                         <textarea name="descritivo" id="descritivo" class="form-control pula" rows="4"
+												  tabindex="11"
 												  @if($errors->has('descritivo')) style="border:1px solid #f56954" @endif>
 											{{ old('descritivo') ? old('descritivo') : $imovel->descritivo }}
 										</textarea>
@@ -173,83 +182,11 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="box box-primary box-solid">
-                                <div class="box-header with-border">
-                                    <h3 class="box-title">Endereço</h3>
-                                </div>
-                                <div class="box-body">
-									<div class="row">
-										<div class="col-md-4">
-											<div class="form-group">
-												<label for="CEP" class="control-label" @if($errors->has('cep')) style="color: #f56954" @endif>CEP</label>
-												<input id="CEP" type="text" class="form-control pula" name="cep"
-                                                       data-mask="99999-999" value="{{ old('cep') ? old('cep') : $imovel->endereco->cep }}" @if($errors->has('cep')) style="border:1px solid #f56954" @endif>
-												@if( $errors->has('cep') )
-													<span style="color: #f56954">{{ $errors->get('cep')[0] }}</span>
-												@endif
-											</div>
-										</div>
-
-										<div class="col-md-8">
-											<div class="form-group">
-												<label for="Logradouro" class="control-label" @if($errors->has('logradouro')) style="color: #f56954" @endif>Logradouro</label>
-												<input id="Logradouro" type="text" class="form-control pula" name="logradouro" value="{{ old('logradouro') ? old('logradouro') : $imovel->endereco->logradouro }}" @if($errors->has('logradouro')) style="border:1px solid #f56954" @endif>
-												@if( $errors->has('logradouro') )
-													<span style="color: #f56954">{{ $errors->get('logradouro')[0] }}</span>
-												@endif
-											</div>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-2">
-											<div class="form-group">
-												<label for="Numero" class="control-label" @if($errors->has('numero')) style="color: #f56954" @endif>Número</label>
-												<input id="Numero" type="text" class="form-control pula" name="numero" value="{{ old('numero') ? old('numero') : $imovel->endereco->numero }}" @if($errors->has('numero')) style="border:1px solid #f56954" @endif>
-												@if( $errors->has('numero') )
-													<span style="color: #f56954">{{ $errors->get('numero')[0] }}</span>
-												@endif
-											</div>
-										</div>
-										<div class="col-md-10">
-											<div class="form-group">
-												<label for="Complemento" class="control-label" @if($errors->has('complemento')) style="color: #f56954" @endif>Complemento</label>
-												<input id="Complemento" type="text" class="form-control pula" name="complemento" value="{{ old('complemento') ? old('complemento') : ($imovel->endereco->complemento ? $imovel->endereco->complemento : '') }}"
-													   @if($errors->has('complemento')) style="border:1px solid #f56954" @endif>
-												@if( $errors->has('complemento') )
-													<span style="color: #f56954">{{ $errors->get('complemento')[0] }}</span>
-												@endif
-											</div>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-4">
-											<div class="form-group">
-												<label for="Bairro" class="control-label" @if($errors->has('bairro')) style="color: #f56954" @endif>Bairro</label>
-												<input id="Bairro" type="text" class="form-control pula" name="bairro" value="{{ old('bairro') ? old('bairro') : $imovel->endereco->bairro }}"
-													   @if($errors->has('bairro')) style="border:1px solid #f56954" @endif>
-												@if( $errors->has('bairro') )
-													<span style="color: #f56954">{{ $errors->get('bairro')[0] }}</span>
-												@endif
-											</div>
-										</div>
-										<div class="col-md-8">
-											<div class="form-group">
-												<label for="CidadeCOD" class="control-label" @if($errors->has('cidade_id')) style="color: #f56954" @endif>Cidade</label>
-												<select name="cidade_id" id="CidadeCOD" class="form-control pula select2" @if($errors->has('cidade_id')) style="border:1px solid #f56954" @endif>
-													<option selected disabled>-------Selecione uma cidade-------</option>
-													@foreach($cidades as $cidade)
-														<option value="{{ $cidade->id }}" {{ old('cidade_id') == $cidade->id ? 'selected' : ($cidade->id == $imovel->endereco->cidade_id ? 'selected' : '') }}>
-															{{ $cidade->descricao }} - {{ $cidade->estado->descricao }}</option>
-													@endforeach
-												</select>
-												@if( $errors->has('cidade_id') )
-													<span style="color: #f56954">{{ $errors->get('cidade_id')[0] }}</span>
-												@endif
-											</div>
-										</div>
-									</div>
-                                </div>
-                            </div>
+                            @component('formularios.enderecos.Endereco',[
+                            	'model'     => $imovel,
+                            	'cidades'   => $cidades,
+                            	'prox_tab'	=> '12'
+                            ])@endcomponent
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
@@ -315,8 +252,10 @@
     </div>
 @endsection
 @section('js')
+	<script src="{{ asset('js/select2-tab-fix/select2-tab-fix.min.js') }}"></script>
     <script>
         $(document).ready(function () {
+        	$('.select2').select2();
             $('#Codigo').focus();
         });
         $('#salvar').on('click', function(e){
---
+++ resources/views/formularios/enderecos/Endereco.blade.php
@@ -0,0 +1,115 @@
+<div class="row">
+	<div class="col-md-12">
+		<div class="box box-primary box-solid">
+			<div class="box-header with-border">
+				<h3 class="box-title">Endereço</h3>
+			</div>
+			<div class="box-body">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="form-group @if($errors->has('cep')) has-error @endif">
+							@component('formularios.String',[
+								'nome'		=> 'cep',
+								'id'		=> 'CEP',
+								'texto'		=> 'CEP',
+								'titulo'	=> 'CEP',
+								'valor'		=> old('cep') ?? $model->endereco->cep,
+								'tabindex'	=> $prox_tab,
+								'atributos'	=> 'data-mask=99999-999'
+							])@endcomponent
+							@if( $errors->has('cep') )
+								<span style="color: #f56954">{{ $errors->get('cep')[0] }}</span>
+							@endif
+						</div>
+					</div>
+
+					<div class="col-md-8">
+						<div class="form-group @if($errors->has('logradouro')) has-error @endif">
+							@component('formularios.String',[
+								'nome'		=> 'logradouro',
+								'id'		=> 'Logradouro',
+								'texto'		=> 'Logradouro',
+								'titulo'	=> 'Logradouro',
+								'valor'		=> old('logradouro') ?? $model->endereco->logradouro,
+								'tabindex'	=> $prox_tab+=1
+							])@endcomponent
+							@if( $errors->has('logradouro') )
+								<span style="color: #f56954">{{ $errors->get('logradouro')[0] }}</span>
+							@endif
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-2">
+						<div class="form-group @if($errors->has('numero')) has-error @endif">
+							@component('formularios.String',[
+								'nome'		=> 'numero',
+								'id'		=> 'Numero',
+								'texto'		=> 'Número',
+								'titulo'	=> 'Número',
+								'valor'		=> old('numero') ?? $model->endereco->numero,
+								'tabindex'	=> $prox_tab+=1
+							])@endcomponent
+							@if( $errors->has('numero') )
+								<span style="color: #f56954">{{ $errors->get('numero')[0] }}</span>
+							@endif
+						</div>
+					</div>
+					<div class="col-md-10">
+						<div class="form-group @if($errors->has('complemento')) has-error @endif">
+							@component('formularios.String',[
+								'nome'		=> 'complemento',
+								'id'		=> 'Complemento',
+								'texto'		=> 'Complemento',
+								'titulo'	=> 'Complemento',
+								'valor'		=> old('complemento') ?? $model->endereco->complemento ?? '',
+								'tabindex'	=> $prox_tab+=1
+							])@endcomponent
+							@if( $errors->has('complemento') )
+								<span style="color: #f56954">{{ $errors->get('complemento')[0] }}</span>
+							@endif
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-4">
+						<div class="form-group @if($errors->has('bairro')) has-error @endif">
+							@component('formularios.String',[
+								'nome'		=> 'bairro',
+								'id'		=> 'Bairro',
+								'texto'		=> 'Bairro',
+								'titulo'	=> 'Bairro',
+								'valor'		=> old('bairro') ?? $model->endereco->bairro,
+								'tabindex'	=> $prox_tab+=1
+							])@endcomponent
+							@if( $errors->has('bairro') )
+								<span style="color: #f56954">{{ $errors->get('bairro')[0] }}</span>
+							@endif
+						</div>
+					</div>
+					<div class="col-md-8">
+						<div class="form-group @if($errors->has('cidade_id')) has-error @endif">
+							@component('formularios.Select',[
+								'id'		=> 'CidadeCOD',
+								'nome'		=> 'cidade_id',
+								'texto'		=> 'Cidade',
+								'classes'	=> 'select2',
+								'tabindex'	=> $prox_tab+=1
+							])
+								<option selected disabled>-------Selecione uma cidade-------</option>
+								@foreach($cidades as $cidade)
+									<option value="{{ $cidade->id }}" {{ old('cidade_id') == $cidade->id ? 'selected' : ($cidade->id == $model->endereco->cidade->id ? 'selected' : '') }}>{{ $cidade->descricao }}
+										- {{ $cidade->estado->descricao }}</option>
+								@endforeach
+							@endcomponent
+							@if( $errors->has('cidade_id') )
+								<span style="color: #f56954">{{ $errors->get('cidade_id')[0] }}</span>
+							@endif
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
